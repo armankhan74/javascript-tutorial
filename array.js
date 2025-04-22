0c572b64fe5bb58780bd162872363b773f129228
armankhan74 <codewitharman7455@gamil.com>
2025-04-22 10:51:57 +0530

    array

---
+++ array.js
@@ -0,0 +1,61 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr.indexOf(5));
+console.log(arr)
+
+arr.indexOf(3);
+console.log(arr.indexOf(4));
+console.log(arr)
+
+let arr2 = arr.join(" ");
+console.log(typeof arr2);
+
+let arr3 = arr2.split(" ");
+console.log(arr3);
+
+console.log( "A",arr);
+let arr4 = arr.slice(0,4);
+console.log(arr);
+console.log( "B",arr4);
+
+let arr5 = arr.splice(1,3);
+console.log( arr);
+console.log( "C",arr5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
